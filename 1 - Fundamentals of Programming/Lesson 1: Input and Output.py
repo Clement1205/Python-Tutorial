9e4befbcdf9313bebc412fa39fd39ae9ca006105
Clement1205 <hxhp6220@leeds.ac.uk>
2024-11-10 23:57:01 +0000

    git commit

--- 1 - Fundamentals of Programming/Lesson 1: Input and Output.py
+++ 1 - Fundamentals of Programming/Lesson 1: Input and Output.py
@@ -28,17 +28,17 @@ The same goes for requesting input from users, we store an input in a variable (
 """
 
 # Let's try!
-print('-' * 40) # a way of printing special symbols, here it's just seperating the outputs when you run so it looks better
+print('-' * 40) # a way of printing special symbols, here it's just seperating the outputs when you run so it looks better.
 
 print("bye")
 
 text = "hi"
 print(text)  # with each print, the output is automatically printed on the next line.
 
-print(text + " Mimosa!")  # note that I added a space in front of your name, as variables do not add a space bar for you
+print(text + " Mimosa!")  # note that I added a space in front of your name, as variables do not add a space bar for you.
 
 text_with_space = " hi "
-print(text_with_space + "Mimosa!") # now my new text has a space, so when i run it will automatically add a space
+print(text_with_space + "Mimosa!") # now my new text has a space, so when i run it will automatically add a space.
 
 print('-' * 40)
 
@@ -46,4 +46,4 @@ user_input = input(text)
 print(user_input)
 
 name = input("What is your name? ")
-print(f"Welcome home {name}!") # will teach this in lesson 2, this is a f-string, NOT a f-bomb
+print(f"Welcome home {name}!") # will teach this in lesson 2, this is a f-string, NOT a f-bomb.
--- 1 - Fundamentals of Programming/Lesson 2: Variable Types.py
+++ 1 - Fundamentals of Programming/Lesson 2: Variable Types.py
@@ -7,5 +7,16 @@ String (string): "This is a string"
 Lists, Tuples: ["will be covered later!"], ()
 """
 
+integer = 10 # This is called initialisation. You need to store a value into a variable before using it.
+float = 10.2 # do not recommend you creating names like this or similar to operators, this is just for your understanding.
+boolean = True # strongly recommend making meaningful names like absent = True, exam_passed = False.
+string = "hi" 
 
+print(integer + float)
+print(boolean)
+print(string)
 
+print(f"{string}! This is a mixture of variables and strings that I type")
+print(string + "! This is a mixture of variables and strings that I type")
+
+# The format of f-strings are f"", everything you write inside is a string. To add a variable, simply add a {}.
