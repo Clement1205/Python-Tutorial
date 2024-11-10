1d966d914a4136550901cc97727e68a0e7ac03fe
Clement1205 <hxhp6220@leeds.ac.uk>
2024-11-10 02:22:55 +0000

    git commit

---
+++ 1 - Fundamentals of Programming/Lesson 1: Input and Output.py
@@ -0,0 +1,18 @@
+"""
+In the upcoming codes, there will be a structure for you so don't worry too much :)
+
+# means comment, it will not be run after running process
+assert x == "something"   means I assume when i run, string x will be "something". If True, nothing happens; If false,
+assertion error is raised.
+
+These messages are written in docstrings. The program will ignore these while being run.
+"""
+
+# Let's get started!
+
+"""
+There are 2 ways of printing stuff in python: printing a string or printing a variable
+e.g print("Why do people always use Hello World as example lol")
+e.g text = "Why do people always use Hello World as example lol"
+    print(text)
+"""
---
+++ 1 - Fundamentals of Programming/Lesson 2: Variable Types.py
@@ -0,0 +1,11 @@
+"""
+There are 5 commonly used data types in all programming languages,
+Integer (int): only exact numbers (no decimals or fractions)
+Float (float): contains all numbers including integers
+Boolean (bool): True or False
+String (string): "This is a string"
+Lists, Tuples: ["will be covered later!"], ()
+"""
+
+
+
