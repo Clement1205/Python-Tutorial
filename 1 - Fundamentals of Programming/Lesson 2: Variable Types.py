89286968b6c0daac11bebfc0ec17f6fd3e9a09ef
Clement1205 <hxhp6220@leeds.ac.uk>
2024-11-11 00:56:28 +0000

    git commit

--- 1 - Fundamentals of Programming/Lesson 2: Variable Types.py
+++ 1 - Fundamentals of Programming/Lesson 2: Variable Types.py
@@ -3,10 +3,15 @@ There are 5 commonly used data types in all programming languages,
 Integer (int): only exact numbers (no decimals or fractions)
 Float (float): contains all numbers including integers
 Boolean (bool): True or False
-String (string): "This is a string"
+String (str): "This is a string"
 Lists, Tuples: ["will be covered later!"], ()
+
+Casting:
+age = 20
+str(age) casts the number 20 into string (to print it out)
 """
 
+# Let's try!
 integer = 10 # This is called initialisation. You need to store a value into a variable before using it.
 float = 10.2 # do not recommend you creating names like this or similar to operators, this is just for your understanding.
 boolean = True # strongly recommend making meaningful names like absent = True, exam_passed = False.
@@ -15,6 +20,9 @@ string = "hi"
 print(integer + float)
 print(boolean)
 print(string + "\n")
+
+num = float(input("Enter a number: "))
+print(str(num))
 
 print(f"{string}! This is a mixture of variables and strings that I type")
 print(string + "! This is a mixture of variables and strings that I type")
---
+++ 1 - Fundamentals of Programming/Lesson 3: if-else statements.py
@@ -0,0 +1,30 @@
+"""
+if-else statements work similarly like normal life logic. 
+If you study, then you will have good grades. If not (else), you will
+send your grades to Jesus.
+
+if-statement operators:
+
+Note: Indentations are SUPER IMPORTANT in python!
+"""
+
+# Let's try!
+# 1.
+age = input("Please enter your age: ")
+if age > 0 and age < 18: # (age > 0 and age < 18) is a parameter. If it is true, the indented statement will be printed
+    print("Hello kiddo!")
+elif age >= 18 and age <= 100: # elif means else if. if the first parameter is false, it will come here
+    print("Hello adult!")
+else:  # age must be between 1 to 100 (you need to think of the boundaries and think of the constraints)
+    print("You are faking your age!")
+
+print('-' * 40)
+# 2.
+functional = True # True when an app is functional, False when the app is not
+if functional != True or functional == False: # this is just an illustration, both mean the same thing
+    print("Maintenance is needed.")
+
+# no need to write "else:" if you want it to print nothing
+print('-' * 40)
+# 3.
+first_no = input("Please enter the first number")
