63f4d4d4697d803b48911a32ecd5ae82512572c8
Clement1205 <hxhp6220@leeds.ac.uk>
2024-11-12 00:27:17 +0000

    git commit

--- 1 - Fundamentals of Programming/Lesson 4: Match-Case.py
+++ 1 - Fundamentals of Programming/Lesson 4: Match-Case.py
@@ -4,3 +4,21 @@ statements.
 """
 
 # Let's try!
+print("Welcome to Calculator! Please choose one of the options:")
+print("1 - Addition")
+print("2 - Subtraction")
+print("3 - Multiplication")
+print("4 - Division")
+option = input("Please choose an option: ")
+
+match option:
+    case "1": # need double quotation marks because option variable is a STRING, not an integer.
+        print("You chose addition.")
+    case "2":
+        print("You chose subtraction.")
+    case "3":
+        print("You chose multiplication.")
+    case "4":
+        print("You chose division.")
+    case _:
+        print("Invalid option.")
