794572c22309fceb356c2ce8efebf127adf1ee99
Clement1205 <hxhp6220@leeds.ac.uk>
2024-11-13 00:47:27 +0000

    git commit

---
+++ 1 - Fundamentals of Programming/Lesson 6: Lists and Tuples.py
@@ -0,0 +1,20 @@
+"""
+There are 4 ways of storing data. There are lists, tuples, sets and dictionaries! Here are their differences:
+
+1. Lists []: store every variable in a [], modifiable
+2. Tuples (): store every variable in a (), unmodifiable
+3. sets {}: store every variable in a {}, modifiable, unordered (cannot access it using name[0])
+4. dictionaries {}: use key value pairs to link to data, modifiable, no duplicates allowed
+-------------------------------------------------------------------------------------------------------------------------
+Built in list-functions:
+1. append(item): adds an element at the end of the list 
+2. insert(index, item): adds an element at the specified position
+3. pop(index): removes the element at the specified position
+4. pop(): removes the last element
+5. remove(item): removes the item with specified value
+6. index(item): returns the index of the first element with the specified value
+7. clear(): removes all elements from the list
+8. reverse(): reverses the order of the list
+9. sort(): sorts the items in the list alphabetically (does NOT work for numbers)
+-------------------------------------------------------------------------------------------------------------------------
+"""
