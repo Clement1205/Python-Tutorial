8ce0dab4d57b12278850bdbb5794fba4007e6d7a
Clement1205 <hxhp6220@leeds.ac.uk>
2024-12-02 01:35:20 +0000

    git commit

---
+++ Exercises/ex 2-2.py
@@ -0,0 +1,13 @@
+"""
+Write a code that requests the exam scores of English, Research, Maths, and IT from users and prints out the average in 1 decimal
+place.
+
+For example: "
+Enter your English score: 67
+Enter your Research score: 75
+Enter your Maths score: 85
+Enter your IT score: 90
+
+Your average is 79.2 out of 100.
+"
+"""
---
+++ Exercises/ex 2-3.py
@@ -0,0 +1,12 @@
+"""
+Write a code that requests the exam scores of Research, Maths, and IT from users and prints out the average in exact
+integer.
+
+For example: "
+Enter your Research score: 75
+Enter your Maths score: 85
+Enter your IT score: 90
+
+Your average is 83 out of 100.
+"
+"""
